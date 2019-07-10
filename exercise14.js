76a9d60fe8cb1b8d0486a1a3d793fd6456bfd3c1
igun369 <guntoro.gyk@gmail.com>
2019-07-10 08:53:25 +0700

    modifying exercise14

--- exercise14.js
+++ exercise14.js
@@ -23,5 +23,3 @@ function mengelompokkanAngka(arr) {
   console.log(mengelompokkanAngka([1, 2, 3, 4, 5, 6, 7, 8, 9])); // [ [ 2, 4, 8 ], [ 1, 5, 7 ], [ 3, 6, 9 ] ]
   console.log(mengelompokkanAngka([100, 151, 122, 99, 111])); // [ [ 100, 122 ], [ 151 ], [ 99, 111 ] ]
   console.log(mengelompokkanAngka([])); // [ [], [], [] ]
-
-  console.log(mengelompokkanAngka([1, 2, 21, 20, 111]));
