d026ba38c8c4bb1f53737342b88071f610a206f5
igun369 <guntoro.gyk@gmail.com>
2019-07-10 08:56:57 +0700

    modifying exercise15

--- exercise15.js
+++ exercise15.js
@@ -19,6 +19,4 @@ function groupAnimals(animals) {
 
 // TEST CASES
 console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
-// [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
 console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
-// [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] ]
